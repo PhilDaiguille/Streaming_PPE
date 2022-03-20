d2d9e0c3e828a3d54f017f63768c93ca3b577b0d
PhilDaiguille <philippe.delente@gmail.com>
2022-03-20 15:17:38 +0100

    Add JS - modale photos

--- js/app.js
+++ js/app.js
@@ -26,4 +26,38 @@ document.addEventListener("DOMContentLoaded", () => {
 		}
 		body.classList.toggle("light");
 	});
+
+	let el, modal, closed, open_modal, closed_all;
+
+	el = document.querySelectorAll(".grid-picture-large li");
+	modal = document.querySelector(".parent-modale");
+	closed = document.querySelector(".modale button");
+	closed_all = document.querySelector(".modale img");
+
+	/* property elements */
+
+	open_modal = function () {
+		console.log(this.dataset);
+		/* les variables */
+		let image = this.dataset.image;
+		let title = this.dataset.title;
+		let desc = this.dataset.description;
+		let dates = this.dataset.dates;
+		modal.classList.add("modale-active"); /* ajouter la classe active */
+		/* sélectionner les sélecteurs html*/
+		document.querySelector(".modale img").setAttribute("src", image);
+		document.querySelector(".modale .desc h3").innerText = title;
+		document.querySelector(".modale .desc p").innerHTML = `<strong>Description : </strong>${desc}`;
+		document.querySelector(".modale .desc time").innerText = `Année : ${dates}`;
+		document.querySelector(".modale .desc time").setAttribute("datetime", dates);
+	};
+	for (rows of el) {
+		rows.addEventListener("click", open_modal);
+	}
+	closed.addEventListener("click", () => {
+		modal.classList.remove("modale-active");
+	});
+	closed_all.addEventListener("click", () => {
+		modal.classList.remove("modale-active");
+	});
 });
--- js/formulaire.js
+++ js/formulaire.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
 	/*APP*/
-
 	console.log("charged");
 	let date = new Date();
 	let footer = document.querySelector("footer p");
@@ -11,6 +10,11 @@ document.addEventListener("DOMContentLoaded", () => {
 	/*ACCESSIBILITY*/
 	let btn1 = document.querySelector("header ul li:nth-child(1)");
 	let btn2 = document.querySelector("header ul li:nth-child(2)");
+
+	window.addEventListener("load", e => {
+		console.log("load");
+		body.classList.add("loaded");
+	});
 
 	btn1.addEventListener("click", () => {
 		if (btn1.innerHTML === `<span class="material-icons">visibility</span>`) {
@@ -50,7 +54,39 @@ document.addEventListener("DOMContentLoaded", () => {
 			el.classList.add("error");
 		} else {
 			el.innerText = "Votre formulaire a bien été envoyé";
-			el.classList.add("success");
 		}
 	};
+	let el2, modal, closed, open_modal, closed_all;
+
+	el2 = document.querySelectorAll(".grid-picture-large li");
+	modal = document.querySelector(".parent-modale");
+	closed = document.querySelector(".modale button");
+	closed_all = document.querySelector(".modale img");
+
+	/* property elements */
+
+	open_modal = function () {
+		console.log(this.dataset);
+		/* les variables */
+		let image = this.dataset.image;
+		let title = this.dataset.title;
+		let desc = this.dataset.description;
+		let dates = this.dataset.dates;
+		modal.classList.add("modale-active"); /* ajouter la classe active */
+		/* sélectionner les sélecteurs html*/
+		document.querySelector(".modale img").setAttribute("src", image);
+		document.querySelector(".modale .desc h3").innerText = title;
+		document.querySelector(".modale .desc p").innerHTML = `<strong>Description : </strong>${desc}`;
+		document.querySelector(".modale .desc time").innerText = `Année : ${dates}`;
+		document.querySelector(".modale .desc time").setAttribute("datetime", dates);
+	};
+	for (rows of el2) {
+		rows.addEventListener("click", open_modal);
+	}
+	closed.addEventListener("click", () => {
+		modal.classList.remove("modale-active");
+	});
+	closed_all.addEventListener("click", () => {
+		modal.classList.remove("modale-active");
+	});
 });
